7132ac472ee64628471d66ec15ce1a399ac77706
Joy Ahmed <dev.joyahmed@gmail.com>
2024-01-02 23:55:14 +0600

    Update settings.py

--- app_core/settings.py
+++ app_core/settings.py
@@ -132,13 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'blog_users.CustomUser'
 
 
-
-# mail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'joy572064@gmail.com'
-EMAIL_HOST_PASSWORD = 'whrj tulm kdun quka'
-
 LOGIN_URL = 'login'
